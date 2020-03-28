5aeb8ed846aa6c316a76a4fb710a53e7a0c38bc4
Shannon Kehoe <Shannon.kehoe@dtms.de>
2020-03-28 16:44:01 +0100

    implemented initial call to IBM

--- src/workout.py
+++ src/workout.py
@@ -3,7 +3,16 @@ import pyttsx3 as pt
 from random import randint
 import time
 import abc
-from src.excercises import groups
+from excercises import groups
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from credentials import creds
+
+
+AUTHENTICATOR = IAMAuthenticator('{}'.format(creds['apikey']))
+TEXT_TO_SPEECH = TextToSpeechV1(
+    authenticator=AUTHENTICATOR
+)
 
 class Workout():
 	# TODO:
@@ -35,22 +44,33 @@ class Workout():
 		return zip(list_of_exs, num_excercises)
 
 	#uses python's pyttsx3 library to play the workout for you.
-	def _play_workout(self, zipped_t_and_c):
-		list_it = list(zipped_t_and_c) #convert from a zip object to list
-		#issue is here with pyttsx3.  try implementing IBM's text to speech 
-		for i in range(len(list_it)):
-			w = 'Do ' + str(list_it[i][0]) + ' for ' + str(list_it[i][1]) + ' seconds'
-			countdown = [str(i) for i in range(0, list_it[i][1], -1)]
-			engine = pt.init(driverName="nsss")
-			engine.say(w)
-			engine.runAndWait()
-			for ii in range(int(list_it[i][1])):
-				#print(ii)
-				engine = pt.init()
-				engine.say(str(ii))
-				engine.runAndWait()
-				time.sleep(.20)
+	# def _play_workout(self, zipped_t_and_c):
+	# 	list_it = list(zipped_t_and_c) #convert from a zip object to list
+	# 	#issue is here with pyttsx3.  try implementing IBM's text to speech 
+	# 	for i in range(len(list_it)):
+	# 		w = 'Do ' + str(list_it[i][0]) + ' for ' + str(list_it[i][1]) + ' seconds'
+	# 		countdown = [str(i) for i in range(0, list_it[i][1], -1)]
+	# 		engine = pt.init(driverName="nsss")
+	# 		engine.say(w)
+	# 		engine.runAndWait()
+	# 		for ii in range(int(list_it[i][1])):
+	# 			#print(ii)
+	# 			engine = pt.init()
+	# 			engine.say(str(ii))
+	# 			#engine.runAndWait()
+	# 			time.sleep(.20)
 
+	def _get_sound_files(self, zipped_t_and_c):
+		sound_files =[]
+		for ex, count in list(zipped_t_and_c):
+			with open('audio/' + str(ex)+ '_' + str(count) + '.wav', 'wb') as audio_file:
+				#DONT SAVE IT AS A VARIABLE -- the sound file is created, and that is what we want and will play in the next step.
+				try:
+					audio_file.write(TEXT_TO_SPEECH.synthesize(str(ex), voice='en-US_AllisonVoice', accept='audio/wav').get_result().content)
+				except Exception as error:
+					print("Exception retrieving voice file: {}".format(error))
+	def _play_workout(self,zipped_t_and_c):
+		pass
 
 	#Master function that calls all of the others and assembles a routine.  
 	#Call this function for each muscle group you want to include.
@@ -58,6 +78,10 @@ class Workout():
 		g = self._sanitize_inputs(g)
 		workout = self._build_component(number, g)
 		times = self._assign_time(workout)
-		work_this = self._merge_times_and_components(workout, times)
+		record_this = self._merge_times_and_components(workout, times)
 		print("working out go away")
-		self._play_workout(work_this)
+		#self._play_workout(work_this)
+		self._get_sound_files(record_this)
+
+
+Workout().build_workout(3, 'upper_body')
