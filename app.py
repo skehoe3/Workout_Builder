11371c6f56d4b3bd13df62c3d5f93c5712eb9f63
Shannon Kehoe <Shannon.kehoe@dtms.de>
2020-03-29 15:44:39 +0200

    updated doc strings

--- app.py
+++ app.py
@@ -2,23 +2,37 @@ from flask import Flask, render_template, request
 from src.forms.workout_form import WorkoutForm
 from src.workout import Workout
 
-app = Flask(__name__) #create the Flask app
+app = Flask(__name__)  # create the Flask app
 app.config["SECRET_KEY"] = "boom"
 
-@app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
+
+@app.route('/', methods=['GET', 'POST'])  # allow both GET and POST requests
 def index():
-	"""
-	index of the workout builder
-	"""
-	w = WorkoutForm()
-	if request.method == "GET":
-		return render_template("index.html", form=w)
-	if request.method == "POST":
-		Workout().build_workout(w.num_ex.data, w.muscle_group.data)
-		return render_template("index.html", form=w)
-	return "Not implemented"
+    """
+    index of the workout builder
+    """
+    w = WorkoutForm()
+    if request.method == "GET":
+        return render_template("index.html", form=w)
+    if request.method == "POST":
+        Workout().build_workout(w.num_ex.data, w.muscle_group.data)
+        return render_template("index.html", form=w)
+    return "Not implemented"
+
+
+# allow both GET and POST requests
+@app.route('/workingOut', methods=['GET', 'POST'])
+def workingOut():
+    """
+    index of the workout builder
+    """
+    w = WorkoutForm()
+    if request.method == "GET":
+        pass
+    if request.method == "POST":
+        pass
+    return "Not implemented"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
-
+    app.run(debug=True, port=5000)  # run app in debug mode on port 5000
