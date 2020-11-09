ea3a0ee4bd00964a5eb9d4ada0eb1c7defc6cd54
Shannon Kehoe <skehoe3.gatech@gmail.com>
2020-11-09 17:09:08 +0100

    Update excercises.py

--- src/excercises.py
+++ src/excercises.py
@@ -1,5 +1,5 @@
 groups = {
-        'upper_body':['Normal Pushups','tricep dip', 'plank up', 'inchworm', 'plank taps', 'lateral plank walks', 'plank jacks'],
+        'upper_body':['Normal Pushups','tricep dip', 'plank up', 'inchworm', 'plank taps', 'lateral plank walks', 'plank jacks', "bench press"],
 		'lower_body':['lunges', 'reverse lunges', 'squats', 'Plié Squat Calf Raises', 'side lunges', 'single leg raises', 'side leg raises', 'inner leg raises'],
 		'core':['shoulder taps', 'simple plank', 'plank with knee taps', 'plank and hip twists', 'plank and knee swings', 'flutter kicks', 'bicycle kicks', 'leg raises', 'sit up or crunch', 'toe taps', 'jack knife', 'mountain climber', 'scissor switch', 'Russian Twist'],
 		'cardio':['jumping jacks', 'high knees', 'hop in place', 'jog in place', 'speed skater', ]
